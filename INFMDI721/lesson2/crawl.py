8e5ca7e591ec0ac9906515fec8b47e1ef1562eef
charlestherond <therondcharles@gmail.com>
2018-10-10 10:02:02 +0200

    lesson 2

---
+++ INFMDI721/lesson2/crawl.py
@@ -0,0 +1,65 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+
+
+Urls=["https://www.reuters.com/finance/stocks/financial-highlights/LVMH.PA",
+      "https://www.reuters.com/finance/stocks/financial-highlights/AIR.PA",
+      "https://www.reuters.com/finance/stocks/financial-highlights/DANO.PA"]
+
+quarterEndDecembre = pd.DataFrame()
+actionEvolution = pd.DataFrame()
+dividendYield = pd.DataFrame()
+
+def cleanText(txt):
+    return txt.replace("\n","").replace("\t","").replace(" ","")
+
+for url in Urls:
+    #Lecture du web
+    req = requests.get(url)
+    RQ1={}
+    RQ2={}
+    RQ4={}
+    soup = BeautifulSoup(req.content, "lxml")
+    #Nom de la societe
+    titre =soup.select_one("div#sectionTitle h1").text
+
+    #Question 1
+    querry1 = soup.find(text="SALES (in millions)")
+    row1 = querry1.parent.parent.findNext('tr').select('td.data')
+    RQ1["# of Estimates"]=row1[0].string
+    RQ1["Mean"]=row1[1].string
+    RQ1["High"]=row1[2].string
+    RQ1["Low 1 Year Ago"]=row1[3].string
+    RQ1["Company"]= titre[:10]
+
+    quarterEndDecembre = quarterEndDecembre.append([RQ1])
+
+    #Question 2 et 3
+    querry21=soup.find("div",{"class","sectionQuoteDetail"}).span.findNext('span').text
+    querry22=soup.select_one("div.sectionQuoteDetail span.valueContentPercent span").text
+    querry3= soup.find(text="% Shares Owned:").parent.parent.findNext("td").text
+
+    RQ2["Price"]=cleanText(querry21)
+    RQ2["Change rate"]=cleanText(querry22)
+    RQ2["Company"]=titre[:10]
+    RQ2["% Shares Owned"]=cleanText(querry3)
+
+    actionEvolution = actionEvolution.append([RQ2])
+
+    #Question 4
+    querry4= soup.find(text="Dividend Yield").parent.parent.select("td.data")
+    RQ4["Company"]=cleanText(querry4[0].text)
+    RQ4["Sector"]=cleanText(querry4[1].text)
+    RQ4["Indusrty"]=cleanText(querry4[2].text)
+    RQ4["Compagnie"]=titre[:10]
+
+    dividendYield = dividendYield.append([RQ4])
+
+
+
+# Affichage des resulats
+print(quarterEndDecembre.set_index('Company'))
+print(actionEvolution.set_index('Company'))
+print(dividendYield.set_index('Compagnie'))
