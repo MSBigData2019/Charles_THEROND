3fc71bf6bebeed21549642b6201acfc539e00e74
charlestherond <therondcharles@gmail.com>
2018-10-12 14:06:15 +0200

    test2

--- INFMDI721/test2/testcrawling.py
+++ INFMDI721/test2/testcrawling.py
@@ -45,43 +45,10 @@ for url in Urls:
     # print(np.mean(Prix))
     # print(querry1[0].text)
     print(Remise)
+    print()
     print("nombre de remises "+str(len(Remise)))
     print("moyenne des remises "+ str(np.mean(Remise)))
+    print()
+    print("prix max "+str(np.max(Prix)))
     print("prix moyen "+str(np.mean(Prix)))
-    # print(querry1.span.span)
-    # row1 = querry1.parent.parent.findNext('tr').select('td.data')
-    # RQ1["# of Estimates"]=row1[0].string
-    # RQ1["Mean"]=row1[1].string
-    # RQ1["High"]=row1[2].string
-    # RQ1["Low 1 Year Ago"]=row1[3].string
-    # RQ1["Company"]= titre[:10]
-    #
-    # quarterEndDecembre = quarterEndDecembre.append([RQ1])
-    #
-    # #Question 2 et 3
-    # querry21=soup.find("div",{"class","sectionQuoteDetail"}).span.findNext('span').text
-    # querry22=soup.select_one("div.sectionQuoteDetail span.valueContentPercent span").text
-    # querry3= soup.find(text="% Shares Owned:").parent.parent.findNext("td").text
-    #
-    # RQ2["Price"]=cleanText(querry21)
-    # RQ2["Change rate"]=cleanText(querry22)
-    # RQ2["Company"]=titre[:10]
-    # RQ2["% Shares Owned"]=cleanText(querry3)
-    #
-    # actionEvolution = actionEvolution.append([RQ2])
-    #
-    # #Question 4
-    # querry4= soup.find(text="Dividend Yield").parent.parent.select("td[class='data']")
-    # RQ4["Company"]=cleanText(querry4[0].text)
-    # RQ4["Sector"]=cleanText(querry4[1].text)
-    # RQ4["Indusrty"]=cleanText(querry4[2].text)
-    # RQ4["Compagnie"]=titre[:10]
-    #
-    # dividendYield = dividendYield.append([RQ4])
-
-
-
-# Affichage des resulats
-# print(quarterEndDecembre.set_index('Company'))
-# print(actionEvolution.set_index('Company'))
-# print(dividendYield.set_index('Compagnie'))
+    print("prix min "+str(np.min(Prix)))
