3e6efadcf8acbd508cee9cf53ff95380b13129e9
charlestherond <therondcharles@gmail.com>
2018-10-12 14:16:22 +0200

    test2

--- INFMDI721/test2/testcrawling.py
+++ INFMDI721/test2/testcrawling.py
@@ -15,29 +15,31 @@ def cleanText(txt):
     return txt.replace("\n","").replace("\t","").replace(" ","")
 
 for url in Urls:
-    #Lecture du web
-    req = requests.get(url)
-    RQ1={}
-    # RQ2={}
-    # RQ4={}
-    print(url)
-    soup = BeautifulSoup(req.content, "lxml")
-    # #Nom de la societe
-    # titre =soup.select_one("div#sectionTitle h1").text
-    #
-    # #Question 1
-    querry1 = soup.select("span.darty_prix")
-    querry2 = soup.select("p.darty_prix_barre_remise")
     Remise=[]
     Prix=[]
-    for prix in querry1:
-        try:
-            Prix.append(float(str(prix.text).replace(",",".").replace("€","").replace("*","").replace(' ',"")))
-        except:
-            print(str(prix.text).replace(",",".").replace("€","").replace("*","").replace('\\xa',""))
+    for o in ['','&o=200','&o=400',]:
+        #Lecture du web
+        req = requests.get(url+o)
+        RQ1={}
+        # RQ2={}
+        # RQ4={}
+        print(url+o)
+        soup = BeautifulSoup(req.content, "lxml")
+        # #Nom de la societe
+        # titre =soup.select_one("div#sectionTitle h1").text
+        #
+        # #Question 1
+        querry1 = soup.select("span.darty_prix")
+        querry2 = soup.select("p.darty_prix_barre_remise")
 
-    for remise in querry2:
-        Remise.append(int(str(remise.text).replace("%","").replace(" ","")))
+        for prix in querry1:
+            try:
+                Prix.append(float(str(prix.text).replace(",",".").replace("€","").replace("*","").replace(' ',"")))
+            except:
+                print(str(prix.text).replace(",",".").replace("€","").replace("*","").replace('\\xa',""))
+
+        for remise in querry2:
+            Remise.append(int(str(remise.text).replace("%","").replace(" ","")))
     print(Prix)
     print(Remise)
     print()
