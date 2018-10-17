bcbcbabad4e46b80bcdc0930f4702abb31f7b89c
charlestherond <therondcharles@gmail.com>
2018-10-17 21:49:30 +0200

    test2

--- INFMDI721/lesson3/gitextract.py
+++ INFMDI721/lesson3/gitextract.py
@@ -7,7 +7,7 @@ Urls="https://gist.github.com/paulmillr/2657075"
 
 # Initialisation de rendu
 profil_df = pd.DataFrame()
-head = {'Authorization': 'token {}'.format('e6deb30a9e5e1b0fa3731b631576f285daaa889a')}
+myhead = {'Authorization': 'token {}'.format('b349646a73be61ed820d699cf41e47501d54dcbb')}
 
 # Fonction pour effacer les caracteres genants
 def cleanText(txt):
@@ -20,15 +20,18 @@ soup = BeautifulSoup(req.content, "lxml")
 querry1  = soup.select('table tbody tr td a ')
 for result in querry1:
     if result.text==result['href'][-(len(result.text)):] and not "http" in result.text:
-        print(result.text)
         line={}
         line["NOM"] = result.text
         line["RANK"] = str(result.parent.parent.th.string).replace("#","")
-        get_num_repo = requests.get("https://api.github.com/users/"+result.text, headers=head)
+        get_num_repo = requests.get("https://api.github.com/users/" + result.text, headers=myhead)
         num_repo = 0
-        num_repo += json.loads(get_num_repo.content)["public_repos"]
+        #print(json.loads(get_num_repo.content))
+        num_repo += int(json.loads(get_num_repo.content)["public_repos"] )
+        if num_repo == 0:
+            num_repo+=1
+
         line["NREPO"]=num_repo
-        response = requests.get("https://api.github.com/users/"+result.text+"/repos?per_page="+str(num_repo), headers=head)
+        response = requests.get("https://api.github.com/users/" + result.text +"/repos?per_page=" + str(num_repo), headers=myhead)
         user_repo = json.loads(response.content)
         sum = 0
         for item in user_repo:
@@ -36,17 +39,9 @@ for result in querry1:
         line["Sum_Star"]=sum
         line["moyenne"]=float(sum)/float(num_repo)
         profil_df=profil_df.append([line])
+        print(line)
 
-print(profil_df.head())
+print(profil_df.sort_values(by='moyenne',ascending=False).head())
 
 
 
-get_num_repo = requests.get("https://api.github.com/users/onevcat", headers=head)
-num_repo = json.loads(get_num_repo.content)["public_repos"]
-
-response = requests.get("https://api.github.com/users/onevcat/repos?per_page="+str(num_repo), headers=head)
-user_repo=json.loads(response.content)
-
-for item in user_repo:
-    print(item['full_name'])
-    print(item['stargazers_count'])
